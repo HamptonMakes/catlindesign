655fec41244191ef68cd5c9dca6a941ab4f7ce59
Michael JL Catlin <malrase@gmail.com>
2016-05-24 15:11:19 -0700

    Make sure we authorize before we get to admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,18 @@
 class AdminController < ApplicationController
   layout 'admin'
   
+  before_action :authorize
+  
   def index
+  end
+  
+  private
+  
+  def authorize
+    puts session[:user_id]
+    puts "Authorised with session user id above"
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: "Please log in."
+    end
   end
 end
